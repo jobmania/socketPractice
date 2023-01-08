42abed5effbb959d2d39372e5a2f5a31d67431f5
jobmania <xzxz7003@naver.com>
2023-01-09 05:16:00 +0900

    2023-01-09 1st setting <java 11, spring boot 2.7.7>

---
+++ src/main/java/com/example/tcp_ip/TcpIpApplication.java
@@ -0,0 +1,13 @@
+package com.example.tcp_ip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpIpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcpIpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tcp_ip/TcpIpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tcp_ip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcpIpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
